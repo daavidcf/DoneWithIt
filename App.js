663eed40a9d559c62dc4336019d6c345601b17d8
David Carvalho <daavidcf@gmail.com>
2024-03-21 12:42:59 -0300

    Pretty much created the basics of the app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>If this message shows up, I set everything up properly</Text>
       <StatusBar style="auto" />
     </View>
   );
